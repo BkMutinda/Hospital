614dbfbe2c061e6691b31ae0b4fb22fa1e7f0cf1
Bk Mutinda <kbmkyalo@gmail.com>
2025-04-04 00:06:46 +0300

    first1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      hello hospital system.
-    </>
-  )
-}
-
-export default App
-=======
 import { useState } from "react";
 import "./App.css";
-import Nan From "./components/Nav/nav";
 function App() {
 
   <section>
@@ -24,4 +9,3 @@ function App() {
 }
 
 export default App;
->>>>>>> a0a1bf3df52f5b1dc99df6c858874bf49661de33
