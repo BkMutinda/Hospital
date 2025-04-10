3a4d3782888d94d0f5e8d3ee11eb61a58961e80c
Bernard Mutinda <164620456+BkMutinda@users.noreply.github.com>
2025-04-10 23:53:27 +0300

    Update Nav.jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Nav.css"
+
 
 const Nav = () => {
   return(
     <>
-    <section className=" h-fit bg-purple-700 w-full">
-      <div className="text-black  font-semibold text-md  justify-around flex ">
+    <section className="  ">
+      <div className="text-black  font-semibold text-md  justify-around flex items-center bg-green  h-25 ">
         <div className=" h-fit">
-          Logo
+          LOGO
         </div>
-        <div className=" flex justify-between w-[100px] ">
-          <button>
-            Log In
-          </button>
-          <button>
-            Sign Up
-          </button>
+        <div className="">
+          <ul className=" flex gap-10    ">
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>SERVICES</li>
+            <li>CONTACTS</li>
+          </ul>
+        </div>
+        <div className=" flex gap-2">
+          <button  type="button">Sign UP</button>
+          <button type="button">Log In</button>
         </div>
       </div>
     </section>
