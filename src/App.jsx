b9901efbfff03ed8ea36fa71a2fd62d52d49260a
Bk Mutinda <kbmkyalo@gmail.com>
2025-04-10 23:50:55 +0300

    nav1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/Home/Home";
 const App = () => {
   return (
     <>
-      <section className="w-full bg-teal-50 text-primary flex m-auto ">
+      <section className=" bg-teal-50 text-primary flex m-auto ">
         <Home />
       </section>
     </>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Nav from "../Nav/Nav";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
+
 
 const Home = () => {
   return (
     <>
-      <section className="h-screen bg-teal-400 flex w-[80%] m-auto ">
+      <section className="h-screen bg-primary w-full ">
         <Nav />
         <div className="">
 
