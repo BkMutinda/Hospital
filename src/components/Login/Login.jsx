3bdab37cb1abb1d40051c39fc885df1aaa428309
Bk Mutinda <kbmkyalo@gmail.com>
2025-04-15 20:58:54 +0300

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion';
 
 const Login = () => {
+
+  
+  const route = useNavigate();
+  const handleSignup = () => {
+    route('../signup');
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <div className='flex h-screen bg-background  ' >
+      <motion.div
+      initial={{x: -100, opacity: 0}}
+      animate={{x: 30 , opacity: 1}}
+      transition={{duration: 2.0}} 
+      
+      className="m-auto h-fit w-[35%] border-[1px] border-gray-300 py-10 px-10 rounded-sm  ">
+        <form action="" method=''>
+        <h3 className="text-3xl font-semibold border-b-[2px] border-gray-600 pb-2 " >Log In</h3>
+          <div className=" flex mt-5 justify-between  ">
+              <label htmlFor="f_name">First Name</label>
+              <input
+                type="text"
+                className="border-[1px] border-gray-500 rounded-sm "
+                placeholder=" First Name"
+              />
+            </div>
+            <div className="flex mt-5 justify-between ">
+              <label htmlFor="l_name">Last Name</label>
+              <input
+                type="text"
+                className="border-[1px] border-gray-500 rounded-sm  "
+                placeholder=" Last Name"
+              />
+            </div>
+            <div className="flex mt-5 justify-between ">
+              <label htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                className="border-[1px] border-gray-500 rounded-sm  "
+                placeholder="Enter Email"
+              />
+            </div>
+            <div className="flex mt-5 justify-between ">
+              <label htmlFor="id" >
+                ID
+              </label>
+              <input
+                type="number"
+                className="border-[1px] border-gray-500 rounded-sm  "
+                placeholder="Enter ID"
+              />
+            </div>
+            <p className='mt-7  ' >Don't have an account sign up <span className='text-primary hover:cursor-pointer transition-all ease-in-out duration-500 font-semibold ' onClick={handleSignup} >here</span></p>
+            <div className="">
+              <button type="submit" className=" m-auto mt-5 flex justify-center items-center text-2xl bg-primary hover:bg-green py-1 px-4 rounded-md hover:cursor-pointer transition-all ease-in-out duration-500 ">Login</button>
+            </div>
+        </form>
+      </motion.div>
+    </div>
+    </>
   )
 }
 
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./Signup.css";
 import { useNavigate } from "react-router-dom";
-
+import { motion } from "framer-motion";
 const Signup = () => {
-
   const route = useNavigate();
-  const handleLogin = () =>  {
-    route('../login');
-  }
+  const handleLogin = () => {
+    route("../login");
+  };
   return (
     <>
       <div className="h-screen bg-backgound flex ">
-        <div className="border-[1px] border-gray-300 h-[80%] m-auto w-[50%] mt-15 rounded-sm  ">
-          <form action="" method="" className="flex flex-col w-[60%] m-auto mt-15  ">
-            <h3 className="text-3xl font-semibold border-b-[2px] border-gray-600 pb-2 " >Sign Up</h3>
+        <motion.div
+          initial={{ y: -50 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1.0 }}
+          className="border-[1px] border-gray-300 h-fit pb-5 m-auto w-[50%] mt-10 rounded-sm  "
+        >
+          <form
+            action=""
+            method=""
+            className="flex flex-col w-[60%] m-auto mt-15  "
+          >
+            <h3 className="text-3xl font-semibold border-b-[2px] border-gray-600 pb-2 ">
+              Sign Up
+            </h3>
             <div className=" flex mt-5 justify-between  ">
               <label htmlFor="f_name">First Name</label>
               <input
@@ -31,9 +41,7 @@ const Signup = () => {
               />
             </div>
             <div className="flex mt-5 justify-between ">
-              <label htmlFor="email" Email>
-                Email
-              </label>
+              <label htmlFor="email">Email</label>
               <input
                 type="email"
                 className="border-[1px] border-gray-500 rounded-sm  "
@@ -41,13 +49,11 @@ const Signup = () => {
               />
             </div>
             <div className="flex mt-5 justify-between ">
-              <label htmlFor="email" Email>
-                Email
-              </label>
+              <label htmlFor="id">ID</label>
               <input
-                type="email"
+                type="number"
                 className="border-[1px] border-gray-500 rounded-sm  "
-                placeholder="Enter Email"
+                placeholder="Enter ID"
               />
             </div>
             <div className="flex mt-5 justify-between ">
@@ -66,12 +72,25 @@ const Signup = () => {
                 placeholder="Confirm Password"
               />
             </div>
-            <p className="mt-5 font-light ">Already have an account? <span className="text-primary font-semibold hover:cursor-pointer "  onClick={handleLogin}>Log in</span></p>
+            <p className="mt-5 font-light ">
+              Already have an account?{" "}
+              <span
+                className="text-primary font-semibold hover:cursor-pointer "
+                onClick={handleLogin}
+              >
+                Log in
+              </span>
+            </p>
             <div className=" ">
-              <button type="submit" className=" m-auto mt-5 flex justify-center items-center text-2xl bg-primary hover:bg-green py-1 px-4 rounded-md hover:cursor-pointer transition-all ease-in-out duration-500 ">Submit</button>
+              <button
+                type="submit"
+                className=" m-auto mt-5 flex justify-center items-center text-2xl bg-primary hover:bg-green py-1 px-4 rounded-md hover:cursor-pointer transition-all ease-in-out duration-500 "
+              >
+                Submit
+              </button>
             </div>
           </form>
-        </div>
+        </motion.div>
       </div>
     </>
   );
